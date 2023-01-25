07c19315342e4aaee90314894594db77eadc8174
Mauro0800 <mauroamado000@gmail.com>
2023-01-25 01:43:35 -0300

    Agrega menu hamburguesa en mobile

---
+++ public/javascripts/menuHam.js
@@ -0,0 +1,6 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+ menu= document.querySelector('#menu');
+
+ iconoMenu.addEventListener('click', function(){
+    menu.classList.toggle('active10');
+})
