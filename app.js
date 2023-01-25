470d75971b576de0593115e0882ae2898e6d1dec
Nazarena <nvcamarota@gmail.com>
2023-01-24 23:41:50 -0300

    Agrega linkeos de vistas

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.resolve(__dirname,'public')));
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,'views','index.html')));
 app.get("/login",(req,res)=>res.sendFile(path.resolve(__dirname,'views','login.html')));
 app.get("/register",(req,res)=>res.sendFile(path.resolve(__dirname,'views','register.html')));
-app.get("/shopingcart",(req,res)=>res.sendFile(path.resolve(__dirname,'views','shopingcart.html')));
+app.get("/shopping-cart",(req,res)=>res.sendFile(path.resolve(__dirname,'views','shoppingcart.html')));
 app.get("/product-detail",(req,res)=>res.sendFile(path.resolve(__dirname,'views','productdetail.html')));
 app.get("/reset-password",(req,res)=>res.sendFile(path.resolve(__dirname,'views','resetpassword.html')));
 app.get("/dashboard",(req,res)=>res.sendFile(path.resolve(__dirname,'views','dashboard.html')));
