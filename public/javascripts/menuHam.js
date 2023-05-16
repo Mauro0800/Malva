bf6d98fe1903679affcc0bc0f106bc55cdf1d4b7
Mauro0800 <mauroamado000@gmail.com>
2023-05-16 07:54:35 -0300

    Arregla menu hamburguesa

--- public/javascripts/menuHam.js
+++ public/javascripts/menuHam.js
@@ -1,6 +1,11 @@
-const iconoMenu = document.querySelector('#icono-menu'),
+const iconoMenu = document.querySelector('#icono-menu')
+const iconoCerrar = document.querySelector('#cerrar')
  menu= document.querySelector('#menu');
 
  iconoMenu.addEventListener('click', function(){
+    menu.classList.toggle('active10');
+})
+
+ iconoCerrar.addEventListener('click', function(){
     menu.classList.toggle('active10');
 })
