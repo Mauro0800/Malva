f84be4e8f77f2576e8150d6144378663ea803ffc
giulianagutierrez777 <giulianagutierrez777@gmail.com>
2023-06-08 21:11:49 -0300

    cart done!

--- public/javascripts/productCart.js
+++ public/javascripts/productCart.js
@@ -1,31 +1,33 @@
-const btnAddCart = document.querySelector('#btn-addCart')
+const btnAddCart = document.querySelectorAll('.btn-addCart')
 const URL_API_SERVER = "http://localhost:2023/api";
 
-
-btnAddCart.addEventListener('click',async () => {
-  const id = btnAddCart.getAttribute('data-id')
-  try {
-    const objProductId = {
-      productId: id,
-    };
-    const { ok } = await fetch(`${URL_API_SERVER}/cart/addProduct`, {
-      method: "POST",
-      body: JSON.stringify(objProductId),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json());
-  
-    await Swal.fire({
-      title: ok ? "Producto agregado al carrito" : "Debes iniciar sesión",
-      icon: ok ? "success" : "warning",
-      showConfirmButton: false,
-      timer: 1200,
-    });
-  
-    !ok && (location.href = "/users/login");
-  } catch (error) {
-    console.log(error);
-  }
+Array.from(btnAddCart).forEach(btn => {
+  btn.addEventListener('click',async () => {
+    const id = btn.getAttribute('data-id')
+    try {
+      const objProductId = {
+        productId: id,
+      };
+      const { ok } = await fetch(`${URL_API_SERVER}/cart/addProduct`, {
+        method: "POST",
+        body: JSON.stringify(objProductId),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+    
+      await Swal.fire({
+        title: ok ? "Producto agregado al carrito" : "Debes iniciar sesión",
+        icon: ok ? "success" : "warning",
+        showConfirmButton: false,
+        timer: 1200,
+      });
+    
+      !ok && (location.href = "/users/login");
+    } catch (error) {
+      console.log(error);
+    }
+  })
 })
+
 
